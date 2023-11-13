b45696f4e4a4c5e01bbf73a5fc5ea040cb0319fc
Borowiasty <borowiasty.pl@gmail.com>
2023-11-13 16:11:26 +0100

    Some chenges needed for work

--- model_training/training_data_editor.py
+++ model_training/training_data_editor.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 
-for p in Path('.').glob('**/dataset_2/**/*_license*.txt'):
+for p in Path('.').glob('**/dataset_3/**/*.txt'):
     content = p.read_text()
-    if content[0] == '1':
-        while(content.find('1 ') != -1):
-            if(content.find('1 ') == 0):
-                content = '0' + content[1:]
-            else:
-                content = content[:content.find('1 ')-1] + '\n0' + content[content.find('1 ')+1:]
+    if content != '':
+        if content[0] == '1':
+            while(content.find('1 ') != -1):
+                if(content.find('1 ') == 0):
+                    content = '0' + content[1:]
+                else:
+                    content = content[:content.find('1 ')-1] + '\n0' + content[content.find('1 ')+1:]
 
-        file = p.open('w')
-        file.write(content)
+            file = p.open('w')
+            file.write(content)
+print("FINISHED")
