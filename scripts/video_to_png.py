c69dd945bb93c3c00497f514e9cfee561571ec34
Borowiasty <borowiasty.pl@gmail.com>
2023-11-14 12:52:37 +0100

    Created mp4 to png tool

---
+++ scripts/video_to_png.py
@@ -0,0 +1,40 @@
+import Video_splitter
+import Video
+import cv2
+import time
+import os
+import datetime
+
+from_file_source = 'side_1.mp4'
+from_file_source = os.path.dirname(__file__) + from_file_source
+
+static_video = Video_splitter.Video_splitter(from_file_source)  
+
+cur_frame_no = -1
+
+# delcaring var for possible exit
+quit_cam = 0    
+
+while quit_cam == 0:                                                                           # setting framerate
+
+    # based on selected operating mode
+                                                                                                                # video mode
+    cur_frame_no += 1
+
+    if cur_frame_no < static_video.get_count_frames():
+        try:
+            cur_image = static_video.get_frame(cur_frame_no)
+
+            filename = str(datetime.datetime.now())                                                                         # can save detected plate do file
+            filename = filename.replace(' ', '_')
+            filename = filename.replace(':','-')
+            filename = filename.replace('.','-')
+            filename = os.path.dirname(__file__) + '\\cut_out_plates\\' + filename + '.png'
+            cv2.imwrite(filename, cur_image)
+        except:
+            print('Index out of bound for video, emergency exit')
+            quit(1)
+    else:
+        break
+
+print("Done")
