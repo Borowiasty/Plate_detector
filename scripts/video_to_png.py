a3c186190d3242bba266bae6f0919e98504f1678
Borowiasty <borowiasty.pl@gmail.com>
2023-12-21 14:26:23 +0100

    Created loop over films in video_to_png

--- scripts/video_to_png.py
+++ scripts/video_to_png.py
@@ -4,35 +4,38 @@ import cv2
 import time
 import os
 import datetime
+from pathlib import Path
 
-from_file_source = 'side_1.mp4'
-from_file_source = os.path.dirname(__file__) + '\\' + from_file_source
+for p in Path('.').glob('**/cars_films/*.mp4'):
+    from_file_source = str(p)
 
-static_video = Video_splitter.Video_splitter(from_file_source)  
+    static_video = Video_splitter.Video_splitter(from_file_source)  
 
-cur_frame_no = -1
+    cur_frame_no = -1
 
-# delcaring var for possible exit
-quit_cam = 0
+    # delcaring var for possible exit
+    quit_cam = 0
 
-time.sleep(1)
+    time.sleep(1)
 
-while quit_cam == 0:                                                                         
-    cur_frame_no += 1
-    if cur_frame_no < static_video.get_count_frames():
-        try:
-            cur_image = static_video.get_frame(cur_frame_no)
-            if cur_frame_no % 20 == 0:
-                filename = str(datetime.datetime.now())                                                                         # can save detected plate do file
-                filename = filename.replace(' ', '_')
-                filename = filename.replace(':','-')
-                filename = filename.replace('.','-')
-                filename = os.path.dirname(__file__) + '\\cut_frames\\' + filename + '.png'
-                cv2.imwrite(filename, cur_image)
-        except:
-            print('Index out of bound for video, emergency exit')
-            quit(1)
-    else:
-        break
+    while quit_cam == 0:                                                                         
+        cur_frame_no += 1
+        if cur_frame_no < static_video.get_count_frames():
+            try:
+                cur_image = static_video.get_frame(cur_frame_no)
+                if cur_frame_no % 20 == 0:
+                    filename = str(datetime.datetime.now())                                                                         # can save detected plate do file
+                    filename = filename.replace(' ', '_')
+                    filename = filename.replace(':','-')
+                    filename = filename.replace('.','-')
+                    filename = os.path.dirname(__file__) + '\\cut_frames\\' + filename + '.png'
+                    cv2.imwrite(filename, cur_image)
+            except:
+                print('Index out of bound for video, emergency exit')
+                quit(1)
+        else:
+            break
+
+    print("Done step")
 
 print("Done")
