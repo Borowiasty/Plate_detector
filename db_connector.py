4c8e0082ddf602fa5cc2b80070fbfda7a0c2c1b7
Borowiasty <borowiasty.pl@gmail.com>
2024-01-19 19:14:46 +0100

    Creater connector to SQL module

--- db_connector.py
+++ db_connector.py
@@ -3,7 +3,7 @@ import mysql.connector
 from threading import Thread
 
 class db_connector:
-    def __init__(self, host, user, passwd, database):
+    def __init__(self, host, user, passwd, database, lock):
 
         self._db = mysql.connector.connect(
             host = host,
@@ -20,19 +20,24 @@ class db_connector:
 
         self.stopped = False
 
-    def start_db_supervison(self):
-        Thread(target= self._db_supervisor, args= ()).start()
+        self.lock = lock
+
+    def start_db_supervison(self, Plates_local_databe):
+        Thread(target= self._db_supervisor, args= (Plates_local_databe)).start()
 
     def stop_db_supervisor(self):
         self.stopped = True
 
-    def _db_supervisor(self):
+    def _db_supervisor(self, Plates_local_databe):
         while True:
             if self.stopped:
                 break
-            else:
-                #part of code to sync data with database
-                pass
+            else:                                       #part of code to sync data with database
+                self.lock.acquire()
+                bufor = Plates_local_databe[-1]
+                self.lock.release()
+                if self._check_exist(bufor):
+                    pass
 
     def _check_exist(self, plate_no):
         Q = 'SELECT COUNT(plates.plate_id) FROM plate_detector.plates WHERE plates.plate_no = "' + plate_no + '"'
@@ -42,7 +47,8 @@ class db_connector:
             return True
         else:
             return False
-        
+    
+    #def _c             #[TODO]
 
     def _add_fine(self, plate_no):
         Q = 'INSERT INTO `plate_detector`.`fines`(`number_plate`) SELECT plates.plate_id FROM plate_detector.plates WHERE plates.plate_no = "' + plate_no + '"'
